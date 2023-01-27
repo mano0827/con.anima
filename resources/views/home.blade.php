ca5b6b1d55eda360cd5779552d86514d4f8e4ada
mano0827 <au.dugout@iclod.com>
2023-01-27 18:30:59 +0900

    [update]スライドの調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,51 +16,46 @@
 
 <body>
     <div class="site_container">
+
         <div class="header">
-            <div class="header_list">
+            <div class="header_list d-flex flex-row align-items-center justify-content-between">
                 <img class="header_img w-11" src="{{ asset('image/logo/logo_black.png')}}" alt="con animaロゴ">
-                <ul class="list-inline float-end">
-                    <li class="list-inline-item">Top</li>
-                    <li class="list-inline-item">About</li>
-                    <li class="list-inline-item">Service</li>
-                    <li class="list-inline-item">Contact</li>
-                </ul>
+                <div class="list-inline d-flex">
+                    <div class="mx-3">Top</div>
+                    <div class="mx-3">About</div>
+                    <div class="mx-3">Service</div>
+                    <div class="mx-3">Contact</div>
+                </div>
             </div>
-        </div>
-        <div class="slide">
-            <img src="{{ asset('image/common/slide_1.png')}}" class="img-fluid" alt="スライド1枚目">
         </div>
 
         <div class="slide_wrapper">
-            <div class="slide_img">
-                <div class="slide_img_title">
+            <div class="slide_text">
                     <h1>con anima</h1>
-                </div>
-                <div class="slide_img_text">
-                    <p>～日常を紡ぎ、未来を想う～</p>
-                </div>
+                    <h3>～日常を紡ぎ、未来を想う～</h3>
+            </div>
+        </div>
+
+
+
+        <div class="about_wrapper">
+            <div class="about_title">
+                <h2>完全オーダーメイド<br>
+                    音楽映像制作サービス</h2>
             </div>
 
-
-
-            <div class="about_wrapper">
-                <div class="about_title">
-                    <h2>完全オーダーメイド<br>
-                        音楽映像制作サービス</h2>
-                </div>
-
-                <div class="about_text">
-                    <p>「オリジナル楽曲」×「オリジナル映像」で、結婚や記念日などのお祝い事、または大切な人への贈り物として<br>
-                        完全オーダーメイドのミュージックビデオを作成いたします。<br>
-                        付き合うまでのストーリーや普段の何気ない日常生活、いつもは伝えられない感謝な<br>
-                        お客さま自身の伝えたい想いを<br>
-                        オリジナル楽曲とオリジナル映像で届けてみませんか。
-                    </p>
-                </div>
+            <div class="about_text">
+                <p>「オリジナル楽曲」×「オリジナル映像」で、結婚や記念日などのお祝い事、または大切な人への贈り物として<br>
+                    完全オーダーメイドのミュージックビデオを作成いたします。<br>
+                    付き合うまでのストーリーや普段の何気ない日常生活、いつもは伝えられない感謝な<br>
+                    お客さま自身の伝えたい想いを<br>
+                    オリジナル楽曲とオリジナル映像で届けてみませんか。
+                </p>
             </div>
+        </div>
 
 
-            <!-- <div class="movie_wrapper">
+        <!-- <div class="movie_wrapper">
                 <div class="movie-main">
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/i24t9GPkYWo?loop=1&playlist=i24t9GPkYWo&showinfo=0&theme=light&color=white" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
@@ -72,103 +67,103 @@
 
 
 
-            <div class="service_wrapper">
-                <div class="service_title">
-                    <p>サービス内容</p>
+        <div class="service_wrapper">
+            <div class="service_title">
+                <p>サービス内容</p>
+            </div>
+
+            <ul>
+                <li>
+                    <section class="service_section">
+                        <div class="service_section_img">
+                            <img src="" alt="">
+                        </div>
+                        <div class="service_section_content">
+                            <div class="service_section_content_title">
+                                <h3>初回打ち合わせ体験プラン</h3>
+                            </div>
+                            <div class="service_section_content_price">
+                                <p>¥5,000</p>
+                            </div>
+                            <div class="service_section_content_link">
+                                <a href="http://localhost:8888/service">詳細はこちらから</a>
+                            </div>
+                        </div>
+                    </section>
+                </li>
+
+                <li>
+                    <section class="service_section">
+                        <div class="service_section_img">
+                            <img src="" alt="">
+                        </div>
+                        <div class="service_section_content">
+                            <div class="service_section_content_title">
+                                <h3>セミオーダープラン</h3>
+                            </div>
+                            <div class="service_section_content_price">
+                                <p>¥98,000~</p>
+                            </div>
+                            <div class="service_section_content_link">
+                                <a href="http://localhost:8888/service">詳細はこちらから</a>
+                            </div>
+                        </div>
+                    </section>
+                </li>
+
+
+                <li>
+                    <section class="service_section">
+                        <div class="service_section_img">
+                            <img src="" alt="">
+                        </div>
+                        <div class="service_section_content">
+                            <div class="service_section_content_title">
+                                <h3>フルオーダープラン</h3>
+                            </div>
+                            <div class="service_section_content_price">
+                                <p>¥198,000~</p>
+                            </div>
+                            <div class="service_section_content_link">
+                                <a href="http://localhost:8888/service">詳細はこちらから</a>
+                            </div>
+                        </div>
+                    </section>
+                </li>
+
+            </ul>
+
+
+            <div class="contact_wrapper">
+                <div class="contact_title">
+                    <h2>お問合せ</h2>
                 </div>
 
-                <ul>
-                    <li>
-                        <section class="service_section">
-                            <div class="service_section_img">
-                                <img src="" alt="">
-                            </div>
-                            <div class="service_section_content">
-                                <div class="service_section_content_title">
-                                    <h3>初回打ち合わせ体験プラン</h3>
-                                </div>
-                                <div class="service_section_content_price">
-                                    <p>¥5,000</p>
-                                </div>
-                                <div class="service_section_content_link">
-                                    <a href="http://localhost:8888/service">詳細はこちらから</a>
-                                </div>
-                            </div>
-                        </section>
-                    </li>
-
-                    <li>
-                        <section class="service_section">
-                            <div class="service_section_img">
-                                <img src="" alt="">
-                            </div>
-                            <div class="service_section_content">
-                                <div class="service_section_content_title">
-                                    <h3>セミオーダープラン</h3>
-                                </div>
-                                <div class="service_section_content_price">
-                                    <p>¥98,000~</p>
-                                </div>
-                                <div class="service_section_content_link">
-                                    <a href="http://localhost:8888/service">詳細はこちらから</a>
-                                </div>
-                            </div>
-                        </section>
-                    </li>
-
-
-                    <li>
-                        <section class="service_section">
-                            <div class="service_section_img">
-                                <img src="" alt="">
-                            </div>
-                            <div class="service_section_content">
-                                <div class="service_section_content_title">
-                                    <h3>フルオーダープラン</h3>
-                                </div>
-                                <div class="service_section_content_price">
-                                    <p>¥198,000~</p>
-                                </div>
-                                <div class="service_section_content_link">
-                                    <a href="http://localhost:8888/service">詳細はこちらから</a>
-                                </div>
-                            </div>
-                        </section>
-                    </li>
-
-                </ul>
-
-
-                <div class="contact_wrapper">
-                    <div class="contact_title">
-                        <h2>お問合せ</h2>
+                <div class="contact_content">
+                    <div class="contact_content_name">
+                        <p class="contact_content_name_title">名前</p>
+                        <input type="text" name="" id="">
                     </div>
 
-                    <div class="contact_content">
-                        <div class="contact_content_name">
-                            <p class="contact_content_name_title">名前</p>
-                            <input type="text" name="" id="">
-                        </div>
+                    <div class="contact_content_email">
+                        <p class="contact_content_email_title">メールアドレス</p>
+                        <input type="text" name="" id="">
+                    </div>
 
-                        <div class="contact_content_email">
-                            <p class="contact_content_email_title">メールアドレス</p>
-                            <input type="text" name="" id="">
-                        </div>
+                    <div class="contact_content_subject">
+                        <p class="contact_content_subject_title">件名</p>
+                        <input type="text" name="" id="">
+                    </div>
 
-                        <div class="contact_content_subject">
-                            <p class="contact_content_subject_title">件名</p>
-                            <input type="text" name="" id="">
-                        </div>
-
-                        <div class="contact_content_message">
-                            <p class="contact_content_message_title">メッセージ</p>
-                            <input type="text" name="" id="">
-                        </div>
+                    <div class="contact_content_message">
+                        <p class="contact_content_message_title">メッセージ</p>
+                        <input type="text" name="" id="">
                     </div>
                 </div>
             </div>
-
         </div>
+
+    </div>
 
 </body>
 
